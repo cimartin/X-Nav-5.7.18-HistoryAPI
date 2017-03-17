e25c30e4346229396c46286c949576cb5ad8ce17
cimartin <c.martinher@outlook.es>
2017-03-17 09:59:19 +0100

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -2,7 +2,7 @@
 function swapPhoto(href) {
   var req = new XMLHttpRequest();
   req.open("GET",
-  "http://localhost:8000/gallery/" +
+  "https://aubedac.github.io/X-Nav-5.7.18-HistoryAPI/gallery/" +
              href.split("/").pop(),
            false);
   req.send(null);
